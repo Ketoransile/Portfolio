2702423ebd0be5081c399ea01cfbb3601ade9fef
Ketoran Sileshi <abdisileshi123@gmail.com>
2025-03-02 19:31:39 +0300

    Refactor Home component layout for improved structure and styling

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,12 +3,12 @@ import { Spotlight } from "@/components/ui/Spotlight";
 
 export default function Home() {
   return (
-    <div className="relative bg-black-100 flex justify-center items-center flex-col overflow-hidden mx-auto sm:px-10 px-5 max-lg:pt-20">
-      <main className="min-h-screen">
+    <div className="min-h-screen relative bg-black-100 flex justify-center items-center flex-col overflow-hidden mx-auto sm:px-10 px-5 max-lg:pt-20">
+      <main className="">
         <div className="">
           <Spotlight duration={3} />
         </div>
-        <div className="min-h-screen w-full dark:bg-black-100 bg-white  dark:bg-grid-white/[0.06] bg-grid-black/[0.2]  flex items-center justify-center absolute top-0 left-0">
+        <div className="h-full w-full dark:bg-black-100 bg-white  dark:bg-grid-white/[0.06] bg-grid-black/[0.2]  flex items-center justify-center absolute top-0 left-0">
           <div className="absolute pointer-events-none inset-0 flex items-center justify-center dark:bg-black bg-white [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)]" />
         </div>{" "}
         <Hero />
