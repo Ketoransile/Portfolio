98e056c2d7008d5132f9d269e553fc64864ed2c4
Ketoran Sileshi <abdisileshi123@gmail.com>
2025-03-02 20:46:18 +0300

    Refactor About, Layout, and Projects components for improved styling and layout consistency; adjust z-index values and background properties

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -13,7 +13,7 @@ export default function About() {
           <Spotlight duration={3} />
         </div>
         <div className="h-screen w-full dark:bg-black-100 bg-white  dark:bg-grid-white/[0.3] bg-grid-black/[0.2]  flex items-center justify-center absolute top-0 left-0">
-          <div className="absolute pointer-events-none inset-0 flex items-center justify-center dark:bg-black-100 bg-white [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)]" />
+          <div className="min-h-scren absolute pointer-events-none inset-0 flex items-center justify-center dark:bg-black-100 bg-white [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)]" />
         </div>
         <div className="min-h-screen flex flex-col px-5 pt-20 text-slate-200 ">
           <div className="pt-4 flex gap-12 px-20 max-xl:px-2 max-md:px-0 items-center max-xl:grid grid-cols-1 z-10">
--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -201,8 +201,8 @@ const ProjectsPage: React.FC = () => {
         <div>
           <Spotlight duration={3} />
         </div>
-        <div className="h-screen w-full dark:bg-black-100 bg-white dark:bg-grid-white/[0.06] bg-grid-black/[0.2] flex items-center justify-center absolute top-0 left-0">
-          <div className="absolute pointer-events-none inset-0 flex items-center justify-center dark:bg-black bg-white [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)]" />
+        <div className="h-screen w-full dark:bg-black-100 bg-white dark:bg-grid-white/[0.6] bg-grid-black/[0.2] flex items-center justify-center absolute top-0 left-0">
+          <div className="h-full absolute pointer-events-none inset-0 flex items-center justify-center dark:bg-black bg-white [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)]" />
         </div>
         <TextGenerateEffect
           className="max-md:text-xl md:text-3xl lg:text-4xl pt-12 pb-0"
