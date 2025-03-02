e49b069c111473b3c28960f17b8c27e31410c8ca
Ketoran Sileshi <abdisileshi123@gmail.com>
2025-03-02 20:51:15 +0300

    Refactor Testimonials component layout for improved structure and styling; adjust div properties for better responsiveness

--- src/app/testimonials/page.tsx
+++ src/app/testimonials/page.tsx
@@ -4,12 +4,12 @@ import { TextGenerateEffect } from "@/components/ui/text-generate-effect";
 
 export default function AnimatedTestimonialsDemo() {
   return (
-    <div className="min-h-screen relative bg-black-100 flex justify-center items-center flex-col overflow-hidden mx-auto sm:px-10 px-5 z-40">
+    <div className="min-h-screen relative bg-black-100 flex justify-center items-center flex-col overflow-hidden mx-auto sm:px-10 px-5 ">
       <div className="">
         <Spotlight duration={3} />
       </div>
-      <div className="h-screen w-full dark:bg-black-100 bg-white  dark:bg-grid-white/[0.06] bg-grid-black/[0.2]  flex items-center justify-center absolute top-0 left-0">
-        <div className="absolute pointer-events-none inset-0 flex items-center justify-center dark:bg-black bg-white [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)]" />
+      <div className="min-h-screen w-full dark:bg-black-100 bg-white  dark:bg-grid-white/[0.06] bg-grid-black/[0.2]  flex items-center justify-center absolute top-0 left-0">
+        <div className="min-h-screen absolute pointer-events-none inset-0 flex items-center justify-center dark:bg-black bg-white [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)]" />
       </div>{" "}
       {/* <div className="pt-4 flex gap-12 px-20 z-10 items-center"> */}
       <TextGenerateEffect
