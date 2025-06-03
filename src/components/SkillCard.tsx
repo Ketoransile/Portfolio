e55faeb5844b3449a90ebdb6c984609e877a9661
Ketoran Sileshi <abdisileshi123@gmail.com>
2025-06-03 21:44:17 +0300

    feat: add TypeScript interfaces for SkillCard and SkillsList components

--- src/components/SkillCard.tsx
+++ src/components/SkillCard.tsx
@@ -1,7 +1,10 @@
 import Image from "next/image";
 import Link from "next/link";
-
-export const SkillCard = ({ skill }) => {
+import { skill } from "./SkillsList";
+interface skillProps {
+  skill: skill;
+}
+export const SkillCard = ({ skill }: skillProps) => {
   return (
     <Link href={skill.link}>
       <div className="flex flex-col items-center justify-center gap-4 h-24 w-24 border border-gray-600 rounded-2xl p-4 bg-black-100">
--- src/components/SkillsList.tsx
+++ src/components/SkillsList.tsx
@@ -1,6 +1,14 @@
 import Image from "next/image";
 import { SkillCard } from "./SkillCard";
-export const SkillsList = ({ skills }) => {
+export interface skill {
+  title: string;
+  icon: string;
+  link: string;
+}
+interface skillsListProps {
+  skills: skill[];
+}
+export const SkillsList = ({ skills }: skillsListProps) => {
   return (
     <div className="grid max-md:grid-cols-3 grid-cols-4 gap-4">
       {skills.map((skill, id) => (
